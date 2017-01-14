a2b08e1ab03aa553a8729c979461a8cbae2ae6c4
Harold Fritsch <fritschharold@gmail.com>
2017-01-14 16:07:04 +0100

    creation modele news

---
+++ app/src/main/java/com/haroldfritsch/rssfeedaggregator/Model/News.java
@@ -0,0 +1,18 @@
+package com.haroldfritsch.rssfeedaggregator.Model;
+
+/**
+ * Created by fritsc_h on 14/01/2017.
+ */
+
+public class News {
+    private String creator;
+    private String dateCreation;
+    private String description;
+    private String guid;
+    private String id;
+    private String link;
+    private String pubDate;
+    private String sourceId;
+    private String thumbnail;
+    private String title;
+}
